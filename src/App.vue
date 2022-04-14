452a4d833fc781e60f9c9c9aa3e2a469a3060378
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-13 22:26:54 -0500

    feat: Added Articles fetching support.

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,45 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
+
 
   components: {
     HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      //
+      apiKey: 'fecf4feeffa64e4da682e7d268612ce5',
+      articles: [],
+      errors: [],
+    };
+  },
+  created() {
+    console.log('Created');
+    this.getArticlesForSource('bbc-news');
+  },
+
+  methods: {
+
+    // Fetch Articles for given Source
+
+    getArticlesForSource(sourceId) {
+      axios.get(`https://newsapi.org/v2/top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`)
+          .then( response => {
+            this.articles = response.data.articles;
+            console.log(response.data);
+          })
+          .catch(error => {
+            this.errors.push(error);
+            console.log(this.errors);
+          });
+
+    }
+  }
 }
 </script>

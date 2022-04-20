7c1da9d7f2ea2aa0d661ff699008056f77780549
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-19 20:23:39 -0500

    feat: Implemented Footer Content Component.

---
+++ src/components/footer-content.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" no-gutters class="flex-column">
+    <v-col class="ml-3 text-center">
+      Copyright &copy; 2022. ACME Studio
+    </v-col>
+    <v-col class="ml-3 mt-3 text-center">
+      Made with <v-icon icon="mdi-heart" class="text-red"/>
+      using <a class="text-white" href="https://next.vuetifyjs.com/" target="_blank">Vuetify</a>
+      by ACME Development Team
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+
+<style scoped>
+
+</style>

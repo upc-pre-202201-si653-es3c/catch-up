6492e09e6b0102ddecfbf2281ce9c39a2f8e3050
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-19 20:27:19 -0500

    feat: Implemented No Service available scenario component.

---
+++ src/components/unavailable-content.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row class="flex-column text-center">
+    <v-col class="text-h4">News Service unavailable</v-col>
+    <v-col v-for="error in errors" class="text-h6">{{ error }}</v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "unavailable-content",
+  props: {
+    errors: null
+  }
+}
+</script>

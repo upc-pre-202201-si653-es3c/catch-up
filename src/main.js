f986de43a348badbaa2ad6031f88f84f034c3f60
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-22 20:02:06 -0500

    chore: Added i18n multi-language support.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import i18n from "./i18n";
 
 loadFonts().then()
 
 createApp(App)
   .use(vuetify)
+    .use(i18n)
   .mount('#app')

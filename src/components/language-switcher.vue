4df662797a908336cf7c579fe57db090f9083dfb
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-22 20:09:52 -0500

    chore: Added Language Switcher Component.

---
+++ src/components/language-switcher.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn-toggle v-model="$i18n.locale">
+    <v-btn size="small" class="text-white"
+           v-for="lang in languages"
+           :value="lang">{{ lang }}</v-btn>
+  </v-btn-toggle>
+</template>
+
+<script>
+export default {
+  name: "language-switcher",
+  data() {
+    return {
+      languages: ['en', 'es']
+    }
+  }
+}
+</script>
+
